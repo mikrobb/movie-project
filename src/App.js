594f2387ffec7c1e0f913904569904cc9c0a3817
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2021-10-23 10:39:53 +0300

    add some comments

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import { useEffect , useState } from "react";
-import HomePage from './commponents/HomePage'
-import {BrowserRouter as Router,Route} from 'react-router-dom'
-import FavoriteFilms from "./commponents/FavoriteFilms";
+import { useEffect, useState } from 'react';
+import HomePage from './commponents/HomePage';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import FavoriteFilms from './commponents/FavoriteFilms';
 
 function App() {
-  return(
+  return (
     <>
       <Router>
-        <Route exact path ='/'>
-          <HomePage/>
+        <Route exact path="/">
+          <HomePage />
         </Route>
-        <Route path='/favoriteFilms'>
-          <FavoriteFilms/>
+        <Route path="/favoriteFilms">
+          <FavoriteFilms />
         </Route>
       </Router>
-      
     </>
-  )
+  );
 }
 
 export default App;
--- src/commponents/FavoriteFilms.js
+++ src/commponents/FavoriteFilms.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { favoriteMoiveArr } from "./HomePage";
-import { Fragment } from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { favoriteMoiveArr } from './HomePage';
+import { Fragment } from 'react';
 
 let favMovieArr = favoriteMoiveArr;
 
@@ -10,44 +10,52 @@ function setToLocalStorage(key, value) {
 }
 
 export default function FavoriteFilms() {
+  // rewrite to state favMovies setFavoritesMovies([...favoritesMovies, newFilm])
   function addToFav(event) {
     favMovieArr.push({ name: event.target.parentElement.firstChild.innerHTML });
     console.log(favMovieArr);
-    setToLocalStorage("favMovie", favMovieArr);
-    event.target.classList.remove("unHidden");
-    event.target.previousSibling.classList.remove("hidden");
+    setToLocalStorage('favMovie', favMovieArr);
+    event.target.classList.remove('unHidden');
+    event.target.previousSibling.classList.remove('hidden');
   }
 
   function deleteFav(event) {
     favMovieArr = favMovieArr.filter((item) => {
       return item.name !== event.target.parentElement.firstChild.innerHTML;
     });
+
     console.log(favMovieArr);
-    setToLocalStorage("favMovie", favMovieArr);
-    event.target.classList.add("hidden");
-    event.target.nextSibling.classList.add("unHidden");
+    setToLocalStorage('favMovie', favMovieArr);
+    event.target.classList.add('hidden');
+    event.target.nextSibling.classList.add('unHidden');
   }
 
   return (
     <div className="favMovieMain">
-      <div className="titleBlock" style={{ textAlign: "center" }}>
+      <div className="titleBlock" style={{ textAlign: 'center' }}>
         <span>Whatch List:</span>
       </div>
       <div>
-        {favoriteMoiveArr.map((movie, index) => (
-          <Fragment key = {index}>
-            <ul>
-              <li>{movie.name}</li>
-              <button onClick={deleteFav}>Delete Movie</button>
-                <button onClick={addToFav} className="hidden">
-              Return To Fav
-                </button>
-            </ul>
-            
-          </Fragment>
-        ))}
+        {favoriteMoiveArr.map((movie, index) =>
+          //move to separate component
+          {
+            return (
+              <Fragment key={index}>
+                <ul>
+                  <li>{movie.name}</li>
+                  {/* look below how to change classes or text with state variables */}
+                  {/* <button className={isFavorite ? 'in-favorite' : 'not-favorite'} onClick={toggleFavorite}>{isFavorite ? 'Delete Movie' : 'Add Movie'</button> */}
+                  <button onClick={deleteFav}>Delete Movie</button>
+                  <button onClick={addToFav} className="hidden">
+                    Return To Fav
+                  </button>
+                </ul>
+              </Fragment>
+            );
+          },
+        )}
       </div>
-      <div className="linkToHome" style={{ textAlign: "center" }}>
+      <div className="linkToHome" style={{ textAlign: 'center' }}>
         <Link to="/">Go to HomePage</Link>
       </div>
     </div>
